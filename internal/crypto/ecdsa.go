d5ef064117d1935688765f2dd4267ae1228657e3
_whyvrafvr <jsie@trendev.fr>
2022-03-23 14:01:01 +0100

    test ECDSA generator

--- internal/crypto/ecdsa.go
+++ internal/crypto/ecdsa.go
@@ -1,8 +1,26 @@
 package crypto
 
-import "crypto/ecdsa"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 type JWTECDSA struct {
 	k *ecdsa.PrivateKey
 	JWTBase
+}
+
+func NewJWTES256() *JWTECDSA {
+	rng := rand.Reader
+	pvk, err := ecdsa.GenerateKey(elliptic.P256(), rng)
+	if err != nil {
+		fmt.Println("cannot generate ECDSA key")
+		os.Exit(1)
+	}
+	return &JWTECDSA{pvk, JWTBase{jwt.SigningMethodES256}}
 }
--- internal/crypto/ecdsa_test.go
+++ internal/crypto/ecdsa_test.go
@@ -1,1 +1,39 @@
 package crypto
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func TestNewJWTES256(t *testing.T) {
+	j := NewJWTES256()
+	if j.k == nil {
+		t.Errorf("incorrect key, cannot be nil")
+		t.FailNow()
+	}
+	if j.method != jwt.SigningMethodES256 {
+		t.Errorf("incorrect method, got %v, want %v", j.method, jwt.SigningMethodES256)
+		t.FailNow()
+	}
+
+	x509pvk, err := x509.MarshalECPrivateKey(j.k)
+	if err != nil {
+		t.Errorf("error marshaling ECDSA private key: %v", err)
+		t.FailNow()
+	}
+
+	var pvkbuf bytes.Buffer
+	if err := pem.Encode(&pvkbuf, &pem.Block{Type: "PRIVATE KEY", Bytes: x509pvk}); err != nil {
+		t.Errorf("error encoding ECDSA private key: %v", err)
+		t.FailNow()
+	}
+	if pvkbuf.Len() == 0 {
+		t.Errorf("error encoding ECDSA private key: buffer cannot be empty")
+		t.FailNow()
+	}
+
+}
