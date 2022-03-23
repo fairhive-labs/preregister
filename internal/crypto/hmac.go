abc89a459e6d392ccb287f7657bee1adb3eb736c
_whyvrafvr <jsie@trendev.fr>
2022-03-23 12:27:07 +0100

    refactoring with generics

--- internal/crypto/hmac.go
+++ internal/crypto/hmac.go
@@ -1,16 +1,11 @@
 package crypto
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/fairhive-labs/preregister/internal/data"
 	"github.com/golang-jwt/jwt/v4"
 )
-
-type JWTBase struct {
-	method jwt.SigningMethod
-}
 
 type JWTHS struct {
 	secret []byte
@@ -26,44 +21,9 @@ func NewJWTHS512(s string) *JWTHS {
 }
 
 func (j JWTHS) Create(user *data.User, t time.Time) (string, error) {
-	claims := UserClaims{
-		*user,
-		jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(t.Add(10 * time.Minute)),
-			IssuedAt:  jwt.NewNumericDate(t),
-			NotBefore: jwt.NewNumericDate(t),
-			Issuer:    "fairhive.io",
-		},
-	}
-	token := jwt.NewWithClaims(j.method, claims)
-	ss, err := token.SignedString(j.secret)
-	if err != nil {
-		fmt.Printf("error creating token for user %v : %v", *user, err)
-		err = ErrSigningToken
-	}
-	return ss, err
+	return create(user, t, j.method, j.secret)
 }
 
 func (j JWTHS) Extract(token string) (u *data.User, err error) {
-	uclaims := &UserClaims{}
-	tk, err := jwt.ParseWithClaims(token, uclaims, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			fmt.Printf("Unexpected signing method: %v", token.Header["alg"])
-			return nil, jwt.ErrSignatureInvalid
-		}
-		return j.secret, nil
-	})
-
-	if tk.Valid &&
-		uclaims.Address != "" &&
-		uclaims.Email != "" &&
-		uclaims.Type != "" { // mandatory Fields...
-		return data.NewUser(uclaims.Address, uclaims.Email, uclaims.Type), nil
-	}
-	err = ErrInvalidToken
-	return
-}
-
-func (JWTHS) Hash(token string) string {
-	return hash(token)
+	return extract[*jwt.SigningMethodHMAC](token, j.secret)
 }
--- internal/crypto/token.go
+++ internal/crypto/token.go
@@ -16,6 +16,10 @@ type Token interface {
 	Hash(token string) string
 }
 
+type JWTBase struct {
+	method jwt.SigningMethod
+}
+
 type UserClaims struct {
 	data.User
 	jwt.RegisteredClaims
@@ -28,4 +32,47 @@ var (
 
 func hash(token string) string {
 	return fmt.Sprintf("%X", sha3.Sum512([]byte(token)))
+}
+
+func (JWTBase) Hash(token string) string {
+	return hash(token)
+}
+
+func create(user *data.User, t time.Time, m jwt.SigningMethod, k interface{}) (string, error) {
+	claims := UserClaims{
+		*user,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(t.Add(10 * time.Minute)), // seconds
+			IssuedAt:  jwt.NewNumericDate(t),                       // seconds
+			NotBefore: jwt.NewNumericDate(t),                       // seconds
+			Issuer:    "fairhive.io",
+		},
+	}
+	token := jwt.NewWithClaims(m, claims)
+	ss, err := token.SignedString(k)
+	if err != nil {
+		fmt.Printf("error creating token for user %v : %v", *user, err)
+		err = ErrSigningToken
+	}
+	return ss, err
+}
+
+func extract[T *jwt.SigningMethodHMAC | *jwt.SigningMethodECDSA](token string, k interface{}) (u *data.User, err error) {
+	uclaims := &UserClaims{}
+	tk, err := jwt.ParseWithClaims(token, uclaims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(T); !ok {
+			fmt.Printf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return k, nil
+	})
+
+	if tk.Valid &&
+		uclaims.Address != "" &&
+		uclaims.Email != "" &&
+		uclaims.Type != "" { // mandatory Fields...
+		return data.NewUser(uclaims.Address, uclaims.Email, uclaims.Type), nil
+	}
+	err = ErrInvalidToken
+	return
 }
