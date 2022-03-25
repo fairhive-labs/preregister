c71f0a9428ef128d6c67f89e0837ba9da81869f4
_whyvrafvr <jsie@trendev.fr>
2022-03-25 16:39:05 +0100

    refactor sendEmail()

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -46,26 +46,21 @@ func NewMailer(from, password, host string, port int) *SmtpMailer {
 	}, t}
 }
 
-func (m *SmtpMailer) SendActivationEmail(e, u, h string) (err error) {
+func sendEmail(m *SmtpMailer, e, s, n string, data any) (err error) {
 	to := []string{e}
 	auth := smtp.PlainAuth("", m.from, m.password, m.host)
 
 	var body bytes.Buffer
 	body.Write([]byte(fmt.Sprintf(`From: %s
 To: %s
-Subject: fairhive - preregistration
+Subject: %s
 %s
 `, "no_reply@fairhive-labs.com",
 		e,
+		s,
 		headers)))
 
-	m.t.ExecuteTemplate(&body, "emailActivation", struct {
-		Hash string
-		Url  string
-	}{
-		Hash: h,
-		Url:  u,
-	})
+	m.t.ExecuteTemplate(&body, n, data)
 
 	fmt.Println("Sending email...")
 	r := 3
@@ -77,11 +72,24 @@ Subject: fairhive - preregistration
 		fmt.Printf("failed %d/%d, retrying in 1s...\n", i+1, r)
 		time.Sleep(1 * time.Second)
 	}
+	return
+}
+
+func (m *SmtpMailer) SendActivationEmail(e, u, h string) (err error) {
+	err = sendEmail(m, e, "fairhive - preregistration", "emailActivation",
+		struct {
+			Hash string
+			Url  string
+		}{
+			Hash: h,
+			Url:  u,
+		})
 	logEmailSent(e, h, err)
 	return
 }
 
 func (m *MockSmtpMailer) SendActivationEmail(e, u, h string) (err error) {
+	// do nothing just log
 	logEmailSent(e, h, err)
 	return
 }
