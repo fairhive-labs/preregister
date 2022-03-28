bda9864d14eed9f08200007f34eb46344ca5f3be
_whyvrafvr <jsie@trendev.fr>
2022-03-28 15:53:25 +0200

    impl. + test Encrypt()

--- internal/crypto/cipher.go
+++ internal/crypto/cipher.go
@@ -1,8 +1,11 @@
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"io"
 )
 
@@ -13,4 +16,24 @@ func GenerateKey(size int) (s string, err error) {
 	}
 	s = hex.EncodeToString(b)
 	return
+}
+
+func Encrypt(plaintext, ks string) (string, error) {
+	k, err := hex.DecodeString(ks)
+	if err != nil {
+		return "", err
+	}
+	c, err := aes.NewCipher(k)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", gcm.Seal(nonce, nonce, []byte(plaintext), nil)), nil
 }
--- internal/crypto/cipher_test.go
+++ internal/crypto/cipher_test.go
@@ -5,12 +5,20 @@ import (
 	"testing"
 )
 
+var ks = map[int]string{
+	32: "5a2c2e716f228ebdc8fa23d77e5a9ce8f3d46fe7944f439e8ea034832ab3a449",
+	24: "42c12ae3b1f3bc00bb95ae635b4abbf2d18c5fb3b5e3093c",
+	16: "a95c3bc19469a9cd8b0cf4d09dc04818",
+}
+
 func TestGenerateKey(t *testing.T) {
 	tt := []struct {
 		s int
 		l int
 	}{
 		{32, 64},
+		{24, 48},
+		{16, 32},
 		{10, 20},
 		{12, 24},
 		{128, 256},
@@ -18,6 +26,7 @@ func TestGenerateKey(t *testing.T) {
 	for _, tc := range tt {
 		t.Run(fmt.Sprintf("%d", tc.s), func(t *testing.T) {
 			k, err := GenerateKey(tc.s)
+			t.Logf("GenerateKey(%d)=%s\n", tc.s, k)
 			if err != nil {
 				t.Errorf("incorrect error, got %v, want nil", err)
 				t.FailNow()
@@ -27,5 +36,39 @@ func TestGenerateKey(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestEncrypt(t *testing.T) {
+	plaintext := "this is a secret message"
+
+	tt := []struct {
+		ks  string
+		err error
+	}{
+		{
+			ks[32],
+			nil,
+		},
+		{
+			ks[24],
+			nil,
+		},
+		{
+			ks[16],
+			nil,
+		},
+	}
+
+	for i, tc := range tt {
+		t.Run(fmt.Sprintf("%d", i+1), func(t *testing.T) {
+			es, err := Encrypt(plaintext, tc.ks)
+			if err != tc.err {
+				t.Errorf("incorrect error, got %v, want %v", err, tc.err)
+				t.FailNow()
+			}
+			if "" == es {
+				t.Errorf("incorrect encrypted string, cannot be empty string")
+			}
+		})
+	}
 }
