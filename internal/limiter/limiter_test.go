52f21932c6dd5fd717d216adbae213901cf44e0a
_whyvrafvr <jsie@trendev.fr>
2022-04-04 20:53:36 +0200

    improve burst tests

--- internal/limiter/limiter_test.go
+++ internal/limiter/limiter_test.go
@@ -1,10 +1,15 @@
 package limiter
 
 import (
+	"errors"
+	"fmt"
 	"testing"
+	"time"
 
 	"golang.org/x/time/rate"
 )
+
+var errNotAllowed = errors.New("not allowed")
 
 func TestNew(t *testing.T) {
 	var l rate.Limit = 5.0
@@ -57,21 +62,51 @@ func TestGetAccess(t *testing.T) {
 		t.FailNow()
 	}
 
-	if !lmt.Allow() {
-		t.Errorf("ip %s should be allowed", ip)
-		t.FailNow()
-	}
-
 	if _, ok := limiter.access[ip]; !ok {
 		t.Errorf("access map should contain %s", ip)
 		t.FailNow()
 	}
 
-	// for i := 1; i <= 10; i++ {
-	// 	lmt = limiter.GetAccess(ip)
-	// 	if !lmt.Allow() {
-	// 		t.Errorf("ip %s should be allowed on request %d", ip, i)
-	// 		t.FailNow()
-	// 	}
-	// }
+	if !lmt.Allow() { // consumes 1 token
+		t.Errorf("ip %s should be allowed and consume 1 token", ip)
+		t.FailNow()
+	}
+
+}
+
+func TestBurst(t *testing.T) {
+	ip := "10.10.10.10"
+	tt := []struct {
+		l   rate.Limit
+		b   int
+		err error
+	}{
+		{5.0, 3, nil},
+		{5.0, 5, nil},
+		{10.0, 10, nil},
+		{2.0, 3, errNotAllowed},
+		{99.0, 100, errNotAllowed},
+	}
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("l=%.2f b=%d", tc.l, tc.b), func(t *testing.T) {
+			limiter := New(tc.l, tc.b)
+			n := tc.b + 1
+			simulateNRequests(ip, n, limiter)
+			time.Sleep(time.Second) //pause, add tokens in the bucket
+			if err := simulateNRequests(ip, n, limiter); !errors.Is(err, tc.err) {
+				t.Errorf("incorrect error: got %v, want %v", err, tc.err)
+				t.FailNow()
+			}
+		})
+	}
+}
+
+func simulateNRequests(ip string, n int, limiter *RateLimiter) error {
+	for i := 0; i < n; i++ {
+		lmt := limiter.GetAccess(ip)
+		if !lmt.Allow() && i != limiter.burst { // consumes 1 token
+			return errNotAllowed
+		}
+	}
+	return nil
 }
